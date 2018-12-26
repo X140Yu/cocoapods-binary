32edb8f9a15d9770d81fa1b05750c360acad64da
X140Yu <zhaoxinyu1994@gmail.com>
2018-12-26 21:49:37 +0800

    auto corrected by rubocop

--- lib/cocoapods-binary/helper/names.rb
+++ lib/cocoapods-binary/helper/names.rb
@@ -4,34 +4,33 @@
 # - root_spec.name (spec.root_name, targe.pod_name):
 #   aka "pod_name"
 #   the name we use in podfile. the concept.
-# 
+#
 # - target.name:
 #   aka "target_name"
 #   the name of the final target in xcode project. the final real thing.
 #
-# One pod may have multiple targets in xcode project, due to one pod can be used in mutiple 
+# One pod may have multiple targets in xcode project, due to one pod can be used in mutiple
 # platform simultaneously. So one `root_spec.name` may have multiple coresponding `target.name`s.
 # Therefore, map a spec to/from targets is a little complecated. It's one to many.
 #
 
 # Tool to transform Pod_name to target efficiently
 module Pod
-    def self.fast_get_targets_for_pod_name(pod_name, targets, cache)
-        pod_name_to_targets_hash = nil
-        if cache.empty?
-            pod_name_to_targets_hash = targets.reduce({}) do |sum, target|
-                array = sum[target.pod_name] || []
-                array << target
-                sum[target.pod_name] = array
-                sum
-            end
-            cache << pod_name_to_targets_hash
-        else
-            pod_name_to_targets_hash = cache.first
-        end
-        
-        pod_name_to_targets_hash[pod_name] || []
+  def self.fast_get_targets_for_pod_name(pod_name, targets, cache)
+    pod_name_to_targets_hash = nil
+    if cache.empty?
+      pod_name_to_targets_hash = targets.each_with_object({}) do |target, sum|
+        array = sum[target.pod_name] || []
+        array << target
+        sum[target.pod_name] = array
+      end
+      cache << pod_name_to_targets_hash
+    else
+      pod_name_to_targets_hash = cache.first
     end
+
+    pod_name_to_targets_hash[pod_name] || []
+  end
 end
 
 
@@ -40,19 +39,19 @@ end
 
 
 # Target:
-   
+
 #     def pod_name
 #       root_spec.name
 #     end
 
 #     def name
-# 	    pod_name + #{scope_suffix}
-#     end	
+#       pod_name + #{scope_suffix}
+#     end
 
 #     def product_module_name
 #       root_spec.module_name
 #     end
-  
+
 #     def framework_name
 #       "#{product_module_name}.framework"
 #     end
--- lib/cocoapods-binary/rome/build_framework.rb
+++ lib/cocoapods-binary/rome/build_framework.rb
@@ -1,34 +1,32 @@
 require 'fourflusher'
 
-CONFIGURATION = "Release"
+CONFIGURATION = 'Release'.freeze
 PLATFORMS = { 'iphonesimulator' => 'iOS',
               'appletvsimulator' => 'tvOS',
-              'watchsimulator' => 'watchOS' }
+              'watchsimulator' => 'watchOS' }.freeze
 
 #  Build specific target to framework file
 #  @param [PodTarget] target
 #         a specific pod target
 #
-def build_for_iosish_platform(sandbox, 
-                              build_dir, 
+def build_for_iosish_platform(sandbox,
+                              build_dir,
                               output_path,
-                              target, 
-                              device, 
+                              target,
+                              device,
                               simulator,
                               bitcode_enabled,
                               simulator_default_arch)
 
   deployment_target = target.platform.deployment_target.to_s
-  
+
   target_label = target.label # name with platform if it's used in multiple platforms
   Pod::UI.puts "Prebuilding #{target_label}..."
-  
-  other_options = [] 
-  if bitcode_enabled
-    other_options += ['OTHER_CFLAGS="-fembed-bitcode"']
-  end
+
+  other_options = []
+  other_options += ['OTHER_CFLAGS="-fembed-bitcode"'] if bitcode_enabled
   xcodebuild(sandbox, target_label, device, deployment_target, other_options)
-  xcodebuild(sandbox, target_label, simulator, deployment_target, other_options + ["ARCHS=#{simulator_default_arch}",'ONLY_ACTIVE_ARCH=NO'])
+  xcodebuild(sandbox, target_label, simulator, deployment_target, other_options + ["ARCHS=#{simulator_default_arch}", 'ONLY_ACTIVE_ARCH=NO'])
 
   # paths
   target_name = target.name # equals target.label, like "AFNeworking-iOS" when AFNetworking is used in multiple platforms.
@@ -39,29 +37,28 @@ def build_for_iosish_platform(sandbox,
   device_binary = device_framwork_path + "/#{module_name}"
   simulator_binary = simulator_framwork_path + "/#{module_name}"
   return unless File.file?(device_binary) && File.file?(simulator_binary)
-  
+
   # the device_lib path is the final output file path
   # combine the bianries
   tmp_lipoed_binary_path = "#{build_dir}/#{target_name}"
   lipo_log = `lipo -create -output #{tmp_lipoed_binary_path} #{device_binary} #{simulator_binary}`
   puts lipo_log unless File.exist?(tmp_lipoed_binary_path)
-  FileUtils.mv tmp_lipoed_binary_path, device_binary, :force => true
-  
+  FileUtils.mv tmp_lipoed_binary_path, device_binary, force: true
+
   # collect the swiftmodule file for various archs.
   device_swiftmodule_path = device_framwork_path + "/Modules/#{module_name}.swiftmodule"
   simulator_swiftmodule_path = simulator_framwork_path + "/Modules/#{module_name}.swiftmodule"
   if File.exist?(device_swiftmodule_path)
-    FileUtils.cp_r simulator_swiftmodule_path + "/.", device_swiftmodule_path
+    FileUtils.cp_r simulator_swiftmodule_path + '/.', device_swiftmodule_path
   end
 
   # output
   output_path.mkpath unless output_path.exist?
-  FileUtils.mv device_framwork_path, output_path, :force => true
-
+  FileUtils.mv device_framwork_path, output_path, force: true
 end
 
-def xcodebuild(sandbox, target, sdk='macosx', deployment_target=nil, other_options=[])
-  args = %W(-project #{sandbox.project_path.realdirpath} -scheme #{target} -configuration #{CONFIGURATION} -sdk #{sdk} )
+def xcodebuild(sandbox, target, sdk = 'macosx', deployment_target = nil, other_options = [])
+  args = %W[-project #{sandbox.project_path.realdirpath} -scheme #{target} -configuration #{CONFIGURATION} -sdk #{sdk}]
   platform = PLATFORMS[sdk]
   args += Fourflusher::SimControl.new.destination(:oldest, platform, deployment_target) unless platform.nil?
   args += other_options
@@ -72,12 +69,11 @@ end
 
 module Pod
   class Prebuild
-
     # Build the frameworks with sandbox and targets
     #
     # @param  [String] sandbox_root_path
     #         The sandbox root path where the targets project place
-    #         
+    #
     #         [PodTarget] target
     #         The pod targets to build
     #
@@ -85,27 +81,26 @@ module Pod
     #         output path for generated frameworks
     #
     def self.build(sandbox_root_path, target, output_path, bitcode_enabled = false)
-    
-      return unless not target == nil
-    
+      return if target.nil?
+
       sandbox_root = Pathname(sandbox_root_path)
       sandbox = Pod::Sandbox.new(sandbox_root)
       build_dir = self.build_dir(sandbox_root)
 
       # -- build the framework
       case target.platform.name
-      when :ios then build_for_iosish_platform(sandbox, build_dir, output_path, target, 'iphoneos', 'iphonesimulator', bitcode_enabled, "x86_64")
+      when :ios then build_for_iosish_platform(sandbox, build_dir, output_path, target, 'iphoneos', 'iphonesimulator', bitcode_enabled, 'x86_64')
       when :osx then xcodebuild(sandbox, target.label)
       # when :tvos then build_for_iosish_platform(sandbox, build_dir, target, 'appletvos', 'appletvsimulator')
-      when :watchos then build_for_iosish_platform(sandbox, build_dir, output_path, target, 'watchos', 'watchsimulator', true, "i386")
+      when :watchos then build_for_iosish_platform(sandbox, build_dir, output_path, target, 'watchos', 'watchsimulator', true, 'i386')
       else raise "Unsupported platform for '#{target.name}': '#{target.platform.name}'" end
-    
+
       raise Pod::Informative, 'The build directory was not found in the expected location.' unless build_dir.directory?
 
       # # --- copy the vendored libraries and framework
       # frameworks = build_dir.children.select{ |path| File.extname(path) == ".framework" }
       # Pod::UI.puts "Built #{frameworks.count} #{'frameworks'.pluralize(frameworks.count)}"
-    
+
       # pod_target = target
       # consumer = pod_target.root_spec.consumer(pod_target.platform.name)
       # file_accessor = Pod::Sandbox::FileAccessor.new(sandbox.pod_dir(pod_target.pod_name), consumer)
@@ -113,26 +108,24 @@ module Pod
       # frameworks += file_accessor.vendored_frameworks
 
       # frameworks.uniq!
-    
+
       # frameworks.each do |framework|
       #   FileUtils.mkdir_p destination
       #   FileUtils.cp_r framework, destination, :remove_destination => true
       # end
       # build_dir.rmtree if build_dir.directory?
     end
-    
+
     def self.remove_build_dir(sandbox_root)
       path = build_dir(sandbox_root)
       path.rmtree if path.exist?
     end
 
-    private 
-    
+    private_class_method :build_dir
+
     def self.build_dir(sandbox_root)
       # don't know why xcode chose this folder
-      sandbox_root.parent + 'build' 
+      sandbox_root.parent + 'build'
     end
-
   end
 end
-
